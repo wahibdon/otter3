26caf7d74d44898429db082c0fb3d92b8ccba39a
Austin Hibdon <austin.hibdon@gmail.com>
2015-11-16 16:31:51 -0500

    finished with listing and sorting the type of projects as well as pagination.  seriously, though, I need to figure out how to get ALL the stuff in there even with the users that no longer exist.

--- api/php-api.php
+++ api/php-api.php
@@ -28,17 +28,20 @@ switch ($call){
 		echo json_encode($stmt->fetchAll(PDO::FETCH_OBJ));
 		break;
 	case 'job-search':
-		if(!isset($_GET['page']))
+		if(!isset($_GET['page']) || !isset($_GET['status']))
 			break;
+		$status = ($_GET['status'] == 0) ? '%' : $_GET['status'];
 		$lower = ($_GET['page']-1)*50;
-		$upper = $_GET['page']*50;
-		$stmt = $db->prepare("select jobs.*, abbr, users.first, users.last from jobs left join clients on jobs.client_id = clients.id left join users on users.id = jobs.creator order by id DESC limit ?,?");
-		$stmt->bindValue(1, $lower, PDO::PARAM_INT);
-		$stmt->bindValue(2, $upper, PDO::PARAM_INT);
+		$stmt = $db->prepare("select jobs.*, abbr, users.first, users.last from jobs left join clients on jobs.client_id = clients.id left join users on users.id = jobs.creator where job_status like ? order by id DESC limit ?,50");
+		$stmt->bindValue(1, $status);
+		$stmt->bindValue(2, $lower, PDO::PARAM_INT);
 		$stmt->execute();
-		//echo "$lower \n $upper\n\n";
-		//print_r($db->errorInfo());
-		echo json_encode($stmt->fetchAll(PDO::FETCH_OBJ));
+		$jobs = $stmt->fetchAll(PDO::FETCH_OBJ);
+		$stmt = $db->prepare("select count(id) from jobs where job_status like ?");
+		$stmt->bindValue(1, $status);
+		$stmt->execute();
+		$count = $stmt->fetchColumn();
+		echo json_encode([$count, $jobs]);
 		break;
 	default:
 		echo json_encode("false");
